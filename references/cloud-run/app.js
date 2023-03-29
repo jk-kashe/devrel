49dfe65e81cc279a53a92df8b8b80128850c28ac
J.K. Kashe <127425778+jk-kashe@users.noreply.github.com>
2023-03-29 18:06:32 +0000

    sample json service

--- references/cloud-run/app.js
+++ references/cloud-run/app.js
@@ -18,7 +18,39 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`Hello from Apigee`);
+  var jsonObj = {
+    "product": "Live JSON generator",
+    "version": 3.1,
+    "releaseDate": "2014-06-25T00:00:00.000Z",
+    "demo": true,
+    "person": {
+      "id": 12345,
+      "name": "John Doe",
+      "phones": {
+        "home": "800-123-4567",
+        "mobile": "877-123-1234"
+      },
+      "email": [
+        "jd@example.com",
+        "jd@example.org"
+      ],
+      "dateOfBirth": "1980-01-02T00:00:00.000Z",
+      "registered": true,
+      "emergencyContacts": [
+        {
+          "name": "Jane Doe",
+          "phone": "888-555-1212",
+          "relationship": "spouse"
+        },
+        {
+          "name": "Justin Doe",
+          "phone": "877-123-1212",
+          "relationship": "parent"
+        }
+      ]
+    }
+  }
+  res.status(200).send(JSON.stringify(jsonObj));
 });
 
 const port = process.env.PORT || 8080;
